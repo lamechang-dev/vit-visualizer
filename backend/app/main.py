d36142f55d21dfe0a135f3c8eea3d29a9b9ed36d
Ryosuke Yoshimoto <yoshimoto2107@gmail.com>
2026-05-12 08:17:23 +0900

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,9 @@ model = timm.create_model(
 
 model.eval()
 
+# Linear(in_features=768, out_features=1000, bias=True)
+print(model.head)
+
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     # (H, W, C) → (C, H, W) に変換
@@ -29,11 +32,43 @@ async def predict(file: UploadFile = File(...)):
     # [height][width][channel] = [224][224][3]
     image = Image.open(io.BytesIO(contents)).convert("RGB")
 
+    print(image)
+    # <PIL.Image.Image image mode=RGB size=844x563 at 0x10E50F4D0>
+
+    # [3, 224, 224] → [1, 3, 224, 224] に変換
     x = transform(image).unsqueeze(0)
 
+    # torch.Size([1, 3, 224, 224])
+    print(x.shape)
+
+    # y = logits logitsはまだ確率ではない
+    # 合計1じゃない
+    # マイナスもある
+    # 何でもあり
+    # logts
+        # dog: 12.3
+        # cat: 2.1
+        # car: -4.5
+    # logitsを確率に変換する
+        # softmaxを使う
+        # softmax(logits)
+        # dog: 0.999
+        # cat: 0.001
+        # car: 0.000
     with torch.no_grad():
         y = model(x)
 
+
+    print(y.shape)
+    # torch.Size([1, 1000])
+    print(y)
+    # tensor([[ 4.0559e-02,  1.1820e-01,  6.7152e-01,  2.2492e-01,  2.7555e-01,...
+    
+    # 一番logitsが大きいものを選ぶ
+    # dog: 12.3
+    # cat: 2.1
+    # car: -4.5
+    # なのでdogを選ぶ
     pred = y.argmax().item()
 
     return {
